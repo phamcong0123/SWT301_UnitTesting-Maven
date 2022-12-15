4453dc61ba1d4e5119df9d894a405b088650431e
Cong <phamphucthanhcong01@gmail.com>
2022-12-15 12:14:27 +0700

    update test script

--- src/test/java/org/congpham/mathutil/core/MathUtilTest.java
+++ src/test/java/org/congpham/mathutil/core/MathUtilTest.java
@@ -4,22 +4,58 @@
  */
 package org.congpham.mathutil.core;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  *
  * @author Admin
  */
-public class MathUtilTest {  
-    @Test   
-    public void testFactorialGivenRightArgumentReturnsWell(){
-        assertEquals(2, MathUtil.getFactorial(2));
-        assertEquals(120, MathUtil.getFactorial(5));  
-    }
+public class MathUtilTest {
+
     @Test
-    public void testFactorialGivenWrongArgumentReturnsException(){
-        assertThrows(IllegalArgumentException.class, ()-> MathUtil.getFactorial(-1));
-        assertThrows(IllegalArgumentException.class, ()-> MathUtil.getFactorial(21));
+    public void testFactorialGivenRightArgumentReturnsWell() {
+        assertEquals(2, MathUtil.getFactorial(2));
+        assertEquals(120, MathUtil.getFactorial(5));
     }
+
+    @Test
+    public void testFactorialGivenWrongArgumentReturnsException() {
+        assertThrows(IllegalArgumentException.class, () -> MathUtil.getFactorial(-1));
+        assertThrows(IllegalArgumentException.class, () -> MathUtil.getFactorial(21));
+    }
+
+    @ParameterizedTest(name = "#{index} - Test with n = {0}, expected {1}")
+    @DisplayName("Parameterized test")
+    @MethodSource("initData")
+//    @CsvSource({
+//        "0, 1",
+//        "1, 1",
+//        "2, 2",
+//        "3, 6",
+//        "4, 24",
+//        "5, 120",
+//        "6, 720"
+//    })
+
+    public void testFactorialGivenRightArgumentReturnsWell(int n, long expected) {
+        assertEquals(expected, MathUtil.getFactorial(n));
+    }
+
+    public static Object[][] initData() {
+        return new Integer[][]{
+            {0, 1},
+            {1, 1},
+            {2, 2},
+            {3, 6},
+            {4, 24},
+            {5, 120},
+            {6, 721}
+        };
+    }
+
 }
